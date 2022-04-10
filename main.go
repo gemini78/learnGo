2fc2ee90c43226727a9fae52f1dd20f5dbd51ebf
stebou mendy <stebou8mendy@gmail.com>
2022-04-10 23:37:13 +0200

    Function with manage error

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func sayHello(name string) {
+	fmt.Printf("Bonjour à tous, je m'appelle %v. \n", name)
+}
+
+func sayBye(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("\bErreur: vous avez oublié de spécifier un nom")
+	}
+	byeMessage := fmt.Sprintf("%v s'en va! Bonne soirée.", name)
+	return byeMessage, nil
+}
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(sayBye(""))
 }
