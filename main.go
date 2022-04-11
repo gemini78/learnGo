eade67640f7800510f6c4474686bd4a9eaaf89da
stebou mendy <stebou8mendy@gmail.com>
2022-04-11 07:19:19 +0200

    Add hello.go

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+var x int // global
+
 func main() {
-	fmt.Println("Hello world")
+	x = 5
+	fmt.Println(x)
+	f()
+	showY()
+}
+
+func f() {
+	x := 10 // local
+	fmt.Println(x)
 }
