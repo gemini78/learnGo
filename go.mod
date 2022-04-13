110131734672a65222185b9941e20d95ad786592
Stébou Mendy <stebou@dimilink.com>
2022-04-13 16:09:48 +0200

    Added module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gemini78/mymodule
+
+go 1.15
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gemini78/mymodule/footypes"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var foovar footypes.Foo
+	foovar.TypeInt = 18
+	foovar.TypeString = "Gemini"
+	foovar.TypeBoolean = true
+
+	fmt.Println(foovar)
+
 }
